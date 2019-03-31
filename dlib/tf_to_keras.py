610f079c222cd65f6ceb324d2ed780efb842957f
Hoang Nghia Tuyen <hnt4499@gmail.com>
2019-03-31 14:27:33 +0800

    Update compatibility

--- dlib/tf_to_keras.py
+++ dlib/tf_to_keras.py
@@ -7,48 +7,18 @@
 # - https://github.com/myutwo150/keras-inception-resnet-v2
 
 # ### Pre-trained tensorflow models
-# 
+#
 # - https://github.com/davidsandberg/facenet
 
-# In[1]:
-
-
+import sys
 import os
+import argparse
 import re
 import numpy as np
 import tensorflow as tf
-
-import sys
-sys.path.append('../code/')
 from inception_resnet_v1 import *
+from keras.layers import Input
 
-
-# In[2]:
-
-
-tf_model_dir = '../model/tf/20170512-110547/'
-npy_weights_dir = '../model/keras/npy_weights/'
-weights_dir = '../model/keras/weights/'
-model_dir = '../model/keras/model/'
-
-weights_filename = 'facenet_keras_weights.h5'
-model_filename = 'facenet_keras.h5'
-
-
-# In[3]:
-
-
-os.makedirs(npy_weights_dir, exist_ok=True)
-os.makedirs(weights_dir, exist_ok=True)
-os.makedirs(model_dir, exist_ok=True)
-
-
-# In[4]:
-
-
-# regex for renaming the tensors to their corresponding Keras counterpart
-re_repeat = re.compile(r'Repeat_[0-9_]*b')
-re_block8 = re.compile(r'Block8_[A-Za-z]')
 
 def get_filename(key):
     filename = str(key)
@@ -84,42 +54,51 @@ def extract_tensors_from_checkpoint_file(filename, output_folder):
         arr = reader.get_tensor(key)
         np.save(path, arr)
 
+def main(args):
+    npy_weights_dir = os.path.join(args.output_dir, 'npy_weights')
+    weights_dir = os.path.join(args.output_dir, 'weights')
+    model_dir = os.path.join(args.output_dir, 'model')
 
-# In[5]:
+    weights_filename = 'facenet_keras_weights.h5'
+    model_filename = 'facenet_keras.h5'
 
+    os.makedirs(npy_weights_dir, exist_ok=True)
+    os.makedirs(weights_dir, exist_ok=True)
+    os.makedirs(model_dir, exist_ok=True)
 
-extract_tensors_from_checkpoint_file(tf_model_dir+'model-20170512-110547.ckpt-250000', npy_weights_dir)
+    # regex for renaming the tensors to their corresponding Keras counterpart
+    re_repeat = re.compile(r'Repeat_[0-9_]*b')
+    re_block8 = re.compile(r'Block8_[A-Za-z]')
 
+    extract_tensors_from_checkpoint_file(args.input_dir, npy_weights_dir)
 
-# In[6]:
-
-
-model = InceptionResNetV1()
-# model.summary()
-
-
-# In[7]:
-
-
-print('Loading numpy weights from', npy_weights_dir)
-for layer in model.layers:
-    if layer.weights:
-        weights = []
-        for w in layer.weights:
-            weight_name = os.path.basename(w.name).replace(':0', '')
-            weight_file = layer.name + '_' + weight_name + '.npy'
-            weight_arr = np.load(os.path.join(npy_weights_dir, weight_file))
-            weights.append(weight_arr)
-        layer.set_weights(weights)
-
-print('Saving weights...')
-model.save_weights(os.path.join(weights_dir, weights_filename))
-print('Saving model...')
-model.save(os.path.join(model_dir, model_filename))
+    inputs = tf.placeholder(dtype='float', shape=[None, args.image_size, args.image_size, 3])
+    model, _ = InceptionResNetV1(inputs=inputs)
 
 
-# In[ ]:
+    print('Loading numpy weights from', npy_weights_dir)
+    for layer in model.layers:
+        if layer.weights:
+            weights = []
+            for w in layer.weights:
+                weight_name = os.path.basename(w.name).replace(':0', '')
+                weight_file = layer.name + '_' + weight_name + '.npy'
+                weight_arr = np.load(os.path.join(npy_weights_dir, weight_file))
+                weights.append(weight_arr)
+            layer.set_weights(weights)
 
+    print('Saving weights...')
+    model.save_weights(os.path.join(weights_dir, weights_filename))
+    print('Saving model...')
+    model.save(os.path.join(model_dir, model_filename))
 
+def parse_arguments(argv):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input_dir', type=str, help='Directory pointing to pretrained model, i.e. ckpt-250000 file.')
+    parser.add_argument('output_dir', type=str, help='Output directory.')
+    parser.add_argument('--image_size', type=int,
+        help='Image size (height, width) in pixels.', default=160)
+    return parser.parse_args(argv)
 
-
+if __name__ == '__main__':
+    main(parse_arguments(sys.argv[1:]))
